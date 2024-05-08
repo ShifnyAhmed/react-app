0448517102bfaa2dbe1156d92bff3f2a0936b5d8
ShifnyAhmed <cb007753@students.apiit.lk>
2024-05-08 23:44:00 +0530

    Learning React: Open and close alert based on button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
   const handleOnClickButton = () => {
-    console.log("I Made A Clickable Button Nigga! How Cool Was That?");
+    setAlertVisibility(true);
   };
+  const closeAlertOnCLick = () => {
+    setAlertVisibility(false);
+  };
+
+  const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
-      <Alert>
-        Hello <h1>Mister</h1>
-      </Alert>
+      {alertVisible && (
+        <Alert onClick={closeAlertOnCLick}>
+          Hello <h1>Mister</h1>
+        </Alert>
+      )}
 
       <Button onClick={handleOnClickButton}>Click Me Nigga!</Button>
     </div>
