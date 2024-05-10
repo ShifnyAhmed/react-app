7c769508779a7941e74cfd573506737a4d3cf4d3
ShifnyAhmed <cb007753@students.apiit.lk>
2024-05-10 16:07:35 +0530

    Learning ReactJs: Created a Food Card Using Props and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Card from "./components/Card";
+import FishCurry from "./assets/menu-1.png";
+import Noodles from "./assets/menu-2.png";
+import Biriyani from "./assets/menu-3.png";
+import PaneerCurry from "./assets/menu-4.png";
 
 function App() {
   return (
     <>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card image={FishCurry} name="Fish Curry" price="Rs 650" />
+      <Card image={Noodles} name="Noodles" price="Rs 1299" />
+      <Card image={Biriyani} name="Biriyani" price="1450" />
+      <Card image={PaneerCurry} name="Paneer Curry" price="700" />
+    <Card />
     </>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,25 @@
 import Ramen from "../assets/food1.png";
 
-function Card() {
+interface Props {
+  image: any;
+  name: string;
+  price: string;
+}
+
+const Card = ({ image, name, price }: Props) => {
   return (
     <div className="card">
-      <img className="card-image" src={Ramen} alt="Ramen Pic"></img>
-      <h2 className="card-foodname">Ramen</h2>
-      <p className="card-price">Price : Rs 500</p>
+      <img className="card-image" src={image} alt="Food Pic" />
+      <h2 className="card-foodname">{name}</h2>
+      <p className="card-price">Price : {price}</p>
     </div>
   );
-}
+};
+
+Card.defaultProps = {
+  image: { Ramen },
+  name: "Default",
+  price: "Default",
+};
 
 export default Card;
